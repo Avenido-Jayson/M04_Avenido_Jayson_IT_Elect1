3688fc43799f553fb72c88ac64174fbac05a3308
Avenido-Jayson <avenidojayson782@gmail.com>
2025-09-25 07:53:18 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import CounterApp from './Activity5/CounterApp':
+import ColorChangerApp from './Activity5/ColorChangerApp':
+const App1 =()=> {
+return (
+  <>
+<CounterApp/>
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+{<ColorChangerApp />}
+</>
+);
+};
+export default App;
